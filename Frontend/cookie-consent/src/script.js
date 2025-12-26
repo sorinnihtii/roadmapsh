73abe5b80122be7ee8e8de9d891d592099f07d5d
Sorin <sorinnihtii@gmail.com>
2025-12-26 21:05:29 +0200

    cookie consent project (5)

---
+++ Frontend/cookie-consent/src/script.js
@@ -0,0 +1,29 @@
+function saveConsent(value) {
+  localStorage.setItem("cookieConsent", value);
+}
+
+const consent = localStorage.getItem("cookieConsent");
+const popup = document.getElementById("popup");
+
+if (!consent) {
+  const accept = document.getElementById("accept");
+  const reject = document.getElementById("reject");
+
+  accept.addEventListener("click", () => {
+    popup.classList.remove("animate-popup");
+    popup.classList.add("animate-away");
+    popup.addEventListener("animationend", () => {
+      popup.remove();
+    });
+    saveConsent("accepted");
+  });
+
+  reject.addEventListener("click", () => {
+    popup.classList.remove("animate-popup");
+    popup.classList.add("animate-away");
+    popup.addEventListener("animationend", () => {
+      popup.remove();
+    });
+    saveConsent("rejected");
+  });
+} else popup.remove();
