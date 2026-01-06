e1ff44a75e8f404018bc7566dccffbe07add1c24
Sorin <sorinnihtii@gmail.com>
2026-01-06 22:18:32 +0200

    small fixes

--- Frontend/Intermediate/custom-dropdown/src/script.js
+++ Frontend/Intermediate/custom-dropdown/src/script.js
@@ -5,12 +5,14 @@ const dropdown = document.getElementById("dropdown");
 const listItems = document.querySelectorAll(".item");
 
 header.addEventListener("click", () => {
-  dropdown.classList.toggle("dropdown-open");
+  const open = dropdown.getAttribute("data-open");
+  if (open == "false") dropdown.setAttribute("data-open", "true");
+  else dropdown.setAttribute("data-open", "false");
 });
 
 document.addEventListener("click", (event) => {
   if (!dropdown.contains(event.target) && !header.contains(event.target)) {
-    dropdown.classList.remove("dropdown-open");
+    dropdown.setAttribute("data-open", "false");
   }
 });
 
@@ -18,9 +20,9 @@ listItems.forEach((item) => {
   item.addEventListener("click", () => {
     display.innerHTML = item.children[0].innerHTML;
     listItems.forEach((i) => {
-      i.classList.remove("selected");
+      i.setAttribute("data-selected", "false");
     });
-    item.classList.add("selected");
-    dropdown.classList.remove("dropdown-open");
+    item.setAttribute("data-selected", "true");
+    dropdown.setAttribute("data-open", "false");
   });
 });
